1e347ce36fbaaae9b3f17436597285bb09e2c6ae
Ali Torabi <sr.ali.torabi@gmail.com>
2023-06-24 13:27:25 +0330

    change server reciever type to pointer

--- server/msg_handler.go
+++ server/msg_handler.go
@@ -5,14 +5,14 @@ import (
 	"github.com/aliworkshop/grpc_chat/client/data"
 )
 
-func (uc _server) HandlerPrivateMsg(c client.Client, request *data.Data) {
+func (uc *_server) HandlerPrivateMsg(c client.Client, request *data.Data) {
 	c.WriteJson(request)
 	if cl, ok := uc.clients[request.Id]; ok {
 		cl.WriteJson(request)
 	}
 }
 
-func (uc _server) HandlerChannelMsg(c client.Client, request *data.Data) {
+func (uc *_server) HandlerChannelMsg(c client.Client, request *data.Data) {
 	if channel, ok := uc.channels[request.Id]; ok {
 		if c.GetKey() != channel.Admin {
 			c.Write(&client.WriteRequest{
@@ -26,7 +26,7 @@ func (uc _server) HandlerChannelMsg(c client.Client, request *data.Data) {
 	}
 }
 
-func (uc _server) HandlerGroupMsg(c client.Client, request *data.Data) {
+func (uc *_server) HandlerGroupMsg(c client.Client, request *data.Data) {
 	if group, ok := uc.groups[request.Id]; ok {
 		for _, userId := range group.Members {
 			uc.clients[userId].WriteJson(request)
